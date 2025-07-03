fa8b01d3cb64a20947886147daf2643a9f2e6ca9
Ibrahim <ibrahimelothmani98@gmail.com>
2025-07-03 01:27:46 -1200

    implement workout handler with GET and POST routes for workout management

--- internal/api/workout_handler.go
+++ internal/api/workout_handler.go
@@ -1,1 +1,45 @@
 package api
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"strconv"
+)
+
+type WorkoutHandler struct {
+}
+
+func NewWorkoutHandler() *WorkoutHandler {
+	return &WorkoutHandler{}
+}
+
+func (wh *WorkoutHandler) HandleGetWorkoutByID(w http.ResponseWriter, r *http.Request) {
+	paramsWorkID := chi.URLParam(r, "workID")
+	if paramsWorkID == "" {
+		http.NotFound(w, r)
+		return
+	}
+
+	workID, err := strconv.ParseInt(paramsWorkID, 10, 64)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Work ID: %d\n", workID)
+}
+
+func (wh *WorkoutHandler) HandleCreateWorkout(w http.ResponseWriter, r *http.Request) {
+	paramsWorkID := chi.URLParam(r, "workID")
+	if paramsWorkID == "" {
+		http.NotFound(w, r)
+		return
+	}
+	workID, err := strconv.ParseInt(paramsWorkID, 10, 64)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Work ID: %d\n", workID)
+}
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,19 +1,24 @@
 package app
 
 import (
+	"github.com/ibrahimelothmani/API/internal/api"
 	"log"
 	"net/http"
 	"os"
 )
 
 type Application struct {
-	Logger *log.Logger
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
 }
 
 func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+
+	workoutHandler := api.NewWorkoutHandler()
 	app := &Application{
-		Logger: logger,
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
 	}
 	return app, nil
 }
